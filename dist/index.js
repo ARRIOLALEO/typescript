fdfc48c63cb068298a8a027ceee9479cb776b25a
mario arriola <riblarsv@gmail.com>
2022-08-18 13:18:04 +0600

    typing classes and use never

--- dist/index.js
+++ dist/index.js
@@ -27,3 +27,82 @@ setTimeout(() => {
     endTime = 0;
     endTime = new Date();
 }, RANDOM_WAIT_TIME);
+function displayCar(car) {
+    let str = `${car.brand} ${car.year}`;
+    if (car.battery !== undefined) {
+        str += car.battery;
+    }
+    return str;
+}
+console.log(displayCar({ brand: "toyota", year: 2002, color: "red" }));
+const car = ["hello", "world", 100];
+const cars = [];
+cars.push("mario");
+function data(information) {
+    if (information instanceof Error) {
+        console.log(information);
+    }
+    else {
+        console.log(information);
+    }
+}
+function sums(a, b) {
+    const c = (a + b);
+    return c;
+}
+const myNewButton = document.createElement("button");
+function addClickHandler(ev) {
+    this.disabled = false;
+}
+myNewButton.addEventListener("click", addClickHandler);
+class Car {
+    constructor(name, type, vimNumber) {
+        this.name = name;
+        this.type = type;
+        this.vimNumber = vimNumber;
+        this.name = name;
+        this.type = type;
+        this.vimNumber = vimNumber;
+    }
+    openTheDoorNow() {
+        console.log(`the car ${this.name} , is the type ${this.type}`);
+    }
+}
+const newCar = new Car("12", "45", 155);
+console.log(newCar);
+// newCar.openTheDoorNow(); this one is not accessible here because is privateKey
+class Boat {
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+    }
+    float() {
+        console.log("im floatting");
+    }
+}
+class Truck {
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+    }
+}
+class Plain {
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+    }
+}
+const newTransport = new Plain("jose");
+if (newTransport instanceof Boat) {
+    newTransport.float();
+}
+else if (newTransport instanceof Plain) {
+    console.log("this is a new one");
+}
+else if (newTransport instanceof Truck) {
+    console.log("this is a new data");
+}
+else {
+    const notExist = newTransport;
+}
+// we need to exhausts all the possibilities before to use never method.
--- src/index.ts
+++ src/index.ts
@@ -72,4 +72,58 @@ function addClickHandler(this:HTMLButtonElement,ev:MouseEvent):void{
 myNewButton.addEventListener("click",addClickHandler)
 
 
+class Car{
+    protected openTheDoor:()=>{};
+    constructor(
+        public name:string, 
+        public type:string,
+        private vimNumber:number
+        ){
+        this.name = name;
+        this.type = type;
+        this.vimNumber = vimNumber;
+    }
 
+    private openTheDoorNow():void{
+        console.log(`the car ${this.name} , is the type ${this.type}`)
+    }
+}
+const newCar = new Car("12","45",155)
+
+console.log(newCar)
+// newCar.openTheDoorNow(); this one is not accessible here because is privateKey
+
+class Boat{
+    constructor(public name:string){
+        this.name = name;
+    }
+   public float():any{
+        console.log("im floatting")
+    }
+}
+class Truck{
+    constructor(public name:string){
+        this.name = name;
+    }
+}
+
+class Plain{
+    constructor(public name:string){
+        this.name = name;
+    }
+}
+
+type transportMethod = Plain | Truck | Boat
+
+const newTransport:transportMethod = new Plain("jose")
+if(newTransport instanceof Boat){
+    newTransport.float()
+}else if(newTransport instanceof Plain){
+    console.log("this is a new one")
+} else if(newTransport instanceof Truck ){
+    console.log("this is a new data")
+}else{
+    const notExist:never = newTransport
+}
+
+// we need to exhausts all the possibilities before to use never method.
